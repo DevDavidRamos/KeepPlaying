89a663e564204ff7ff216bea6314a48100bb5e49
DavidRamos <alexramos@live.com.mx>
2023-08-16 15:02:22 -0600

    agregue productos de otras consolas y su busqueda

---
+++ app/src/main/java/com/example/keepplaying/presentation/home_menu/card_zone/nintendo/NintendoViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.keepplaying.presentation.home_menu.card_zone.nintendo
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.keepplaying.firebase.domain.model.Product
+import com.example.keepplaying.firebase.domain.repository.FavoritesRepository
+import com.example.keepplaying.firebase.domain.usecase.GetProductsByNameQueryUseCase
+import com.example.keepplaying.firebase.domain.usecase.GetXboxProductsUseCase
+import com.example.keepplaying.utils.UiState
+import com.example.keepplaying.utils.mapToUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+@HiltViewModel
+class NintendoViewModel  @Inject constructor(
+    private val getXboxProductsUseCase: GetXboxProductsUseCase,
+    private val favoritesRepository: FavoritesRepository,
+    savedStateHandle: SavedStateHandle
+): ViewModel() {
+
+    private val _productsFlow = MutableStateFlow<UiState<List<Product>>?>(null)
+    val productsFlow = _productsFlow.asStateFlow()
+
+    private val query: String = "Nintendo"
+
+    init {
+
+        getXboxProducts(query)
+    }
+
+    private fun getXboxProducts(queryName: String) {
+        viewModelScope.launch {
+            _productsFlow.value = UiState.Loading
+
+            getXboxProductsUseCase(query = queryName).onEach {
+                _productsFlow.value = it.mapToUiState()
+            }.launchIn(viewModelScope)
+        }
+    }
+
+    fun saveFavorite(id: String) {
+        viewModelScope.launch {
+            favoritesRepository.saveFavorite(id)
+        }
+    }
+
+    fun deleteFavorite(id: String) {
+        viewModelScope.launch {
+            favoritesRepository.deleteFavorite(id)
+        }
+    }
+
+}
